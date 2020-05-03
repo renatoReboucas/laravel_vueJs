281591fda7245501927473da13c7dc23d7cc5f5c
Renato Rebouças <renato190698@gmail.com>
2020-05-03 18:11:02 -0300

    layout da lading quase completo

--- resources/views/lading.blade.php
+++ resources/views/lading.blade.php
@@ -2,11 +2,139 @@
 
 @section('content')
 <div class="menu">
-    <div class="container-flex">
-        <img src="public/resources/images/logo.svg" alt="">
+    <div class="container container-flex">
+        <div class="logo"></div>
+    </div>
+</div>
+
+<div class="main">
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="txt">Fast and simple, Unsecured funding.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p class="txt-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <button class="btn-green" type="button">APPLY NOW</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p class="txt-third">Applying doesn’t affect your credit score, and there’s no obligation to accept funding.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="banner">n
+    <div class="container-flex ">
+        <div class="row">
+            <div class="col">
+                <p class="txt-banner-first">Getting <span style="color: #28AD47">capital</span> for your business is complicated</p>
+            </div>
+            <div class="col">
+                <p class="txt-banner-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="second">
+    <div class="container">
+        <div class="row justify-content-center">
+            <p class="second-txt-first">Take the guesswork out of funding</p>
+        </div>
+        <div class="row justify-content-center">
+            <p class="second-txt-second">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.</p>
+        </div>
     </div>
 
+    <div class="row">
+        <div class="col">
+            <div class="info">
+                <div class="img-1"></div>
+                <div class="info-txt">
+                    Funding awarded in just 10 - 21 days so you can move quickly.
+                </div>
+                <div class="img-2"></div>
+                <div class="info-txt">
+                    Funding awarded in just 10 - 21 days so you can move quickly.
+                </div>
+                <div class="img-3"></div>
+                <div class="info-txt">
+                    Funding awarded in just 10 - 21 days so you can move quickly.
+                </div>
+                <div class="img-4"></div>
+                <div class="info-txt">
+                    Funding awarded in just 10 - 21 days so you can move quickly.
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+
+
+<div class="third">
+    <div class="container">
+        <div class="row justify-content-center">
+            <p class="third-txt">Capital for start-ups, small businesses and entrepreneurs</p>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <div class="info">
+                    <div class="img-5" style="position: absolute;"></div>
+                </div>
+            </div>
+            <div class="col content-txt">
+                <p class="third-txt-img" style="margin-bottom: 0px">Start-up funding</p>
+                <p class="third-txt-img-2">Maximize your funding</p>
+                <span class="third-txt-img-2">Receive more funding than you could anywhere else with our strategic, perfectly-timed application process.</span>
+                <button class="btn-green-2" type="button"><span class="button-txt">APPLY NOW</span></button>
+            </div>
+        </div>
+
+        <div class="row third-content-second">
+            <div class="col content-txt" style="margin-top: 114px;">
+                <p class="third-txt-img" style="margin-bottom: 0px">Business funding</p>
+                <p class="third-txt-img-2">Get lenders to compete for you</p>
+                <span class="third-txt-img-2">Receive better rates and higher funding amounts than you can anywhere else with our competitive loan marketplace.</span>
+                <button class="btn-green-2" type="button"><span class="button-txt">APPLY NOW</span></button>
+            </div>
+            <div class="col">
+                <div class="info">
+                    <div class="img-6" style="position: absolute;"></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div class="bedroom">
+    <div class="row">
+        <div class="col">
+            <p class="bedroom-txt-title">Written testimonials</p>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="bedroom-card">
+            <div class="row">
+                <div class="col">
+                    <div class="info-2">
+                        <div class="img-7" style="position: absolute;"></div>
+                    </div>
+                </div>
+                    <p class="">John Neil</p>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 
 
